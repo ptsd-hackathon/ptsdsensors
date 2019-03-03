18f8df5626a6d3ccc001430197bbbe84725f1481
Roman Oxman <oxmanroman@gmail.com>
2019-03-04 00:08:03 +0200

    fix

--- src/controllers/physical-measurements.controller.ts
+++ src/controllers/physical-measurements.controller.ts
@@ -11,8 +11,8 @@ interface PhysicalMeasurementsRequest extends Request {
 
 const router: Router = Router();
 
-router.post('/:userId', (req: PhysicalMeasurementsRequest, res: Response) => {
-  getProfileStatistics(req.params.userId).subscribe(profileStatistics => {
+router.post('/', (req: PhysicalMeasurementsRequest, res: Response) => {
+  getProfileStatistics(req.body.userId).subscribe(profileStatistics => {
     const response: IRisk = calculateRisk(req.body, profileStatistics);
     res.json(response);
   });
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,5 @@
 export interface IUser {
-  email: string;
+  userId: string;
   profile: IUserProfile;
 }
 
